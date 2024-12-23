a72dcbb41c3965c69e04bc6c954cc208a4520280
Naail Safwan S <111448305+OreoBlue@users.noreply.github.com>
2024-12-23 14:09:06 +0700

    Add files via upload

---
+++ Project.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct mahasiswa
+{
+    char nama[50];
+    char nim[50];
+    char prodi[50];
+    char 
+    
+};
+
+
+
+
+int main(){
+    printf("Sela\n");
+
+
+
+
+}
+
